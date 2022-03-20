098d98774382207dbbb141b0b6727d0fc13e6f32
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-03-20 21:18:28 +0530

    Gameplay UI Controller

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -52,7 +52,12 @@ public class GameManager : MonoBehaviour
 
     void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
     {
-        
+        if (scene.name == "Gameplay") ;
+        {
+            Instantiate(characters[CharIndex]);
+            
+            
+        }
     }
 
 
